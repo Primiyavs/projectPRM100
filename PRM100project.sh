4e63c73b673641b0a467c317ea898e42e74516fd
Primiya VS <primiya.vs@gmail.com>
2022-07-21 22:53:24 -0700

    Developer 1 change

---
+++ PRM100project.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+echo 'Developer 1'
+
+for i in $( ls *.txt )
+do
+        extension="${i##*.}"
+
+        filename="${i%.*}"
+        
+        filename+='_'$(date '+%Y-%m-%d')
+        
+        mv $i $filename.$extension
+        
+done    
